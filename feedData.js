bf81adb9da8ddf33b51d271d1d025e7a6d8717b5
David <david@tangentdigitalagency.com>
2023-06-08 13:58:37 -0700

    updated env port logs

--- feedData.js
+++ feedData.js
@@ -83,7 +83,7 @@ async function sendToPlRater(webhookData) {
   return response;
 }
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
